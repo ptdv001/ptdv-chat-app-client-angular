247d079bf6e7eaba01d57435863168b31c2458a3
ptdv001 <peterdev001@hotmail.com>
2018-12-13 22:06:59 -0800

    Adds Home Page component

---
+++ src/app/home-page/home-page-component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pca-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page-component.scss']
+})
+export class HomePageComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+        console.log('home-page component onInit');
+    }
+}
---
+++ src/app/home-page/home-page.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { HomePageRoutingModule } from './home-page-routing.module';
+import { HomePageComponent } from './home-page-component';
+
+@NgModule({
+    imports: [
+        HomePageRoutingModule
+    ],
+    declarations: [
+        HomePageComponent
+    ]
+})
+export class HomePageModule { }
